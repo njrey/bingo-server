3c5e1213052e4e5ae50d1fe3b62f944d58629823
njrey <nolan.reynolds.2@gmail.com>
2021-08-31 21:01:49 -0400

    feat(server): Simple initial server implementation

--- app.py
+++ app.py
@@ -1,33 +1,66 @@
 import psycopg2
-from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import json
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():
   return 'Hello, Docker!'
 
-@app.route('/boards')
+@app.route('/boards', methods=["GET", "POST", "OPTIONS"])
 def get_boards() :
-  conn = psycopg2.connect(host="db", port = 5432, database="bingo_boards", user="postgres", password="postgres")
-  cursor = conn.cursor()
+  if request.method == 'GET':
+    conn = psycopg2.connect(host="db", port = 5432, database="bingo_boards", user="postgres", password="postgres")
+    cursor = conn.cursor()
 
-  cursor.execute('SELECT * FROM "my_boards"')
-  row_headers=[x[0] for x in cursor.description] #this will extract row headers
+    cursor.execute('SELECT * FROM "my_boards"')
+    row_headers=[x[0] for x in cursor.description] #this will extract row headers
 
-  results = cursor.fetchall()
-  print(results)
+    results = cursor.fetchall()
+    print(results)
 
-  json_data=[]
-  for result in results:
-    json_data.append(dict(zip(row_headers,result)))
+    json_data=[]
+    for result in results:
+      json_data.append(dict(zip(row_headers,result)))
 
-  cursor.close()
+    cursor.close()
+    resp = make_response(json.dumps(json_data))
+    resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
 
-  return json.dumps(json_data)
+    return resp
+  elif request.method == 'POST':
+    
+    conn = psycopg2.connect(host="db", port = 5432, database="bingo_boards", user="postgres", password="postgres")
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO "my_boards" (name, squares) VALUES (%s, %s)', ('hey', request.get_json()))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print(request.get_json())
+    resp = make_response(json.dumps(request.get_json()))
+    resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
+  elif request.method == 'PATCH':
+      return "ECHO: PACTH\n"
+
+  elif request.method == 'PUT':
+      return "ECHO: PUT\n"
+
+  elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+  elif request.method == 'OPTIONS':
+      resp = make_response()
+      resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+      resp.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type, Accept, Origin, Authorization'
+      resp.headers['Access-Control-Allow-Methods'] =  'GET, POST, PUT, DELETE, OPTIONS'
+      return resp
+
+
+  
 
 @app.route('/initdb')
 def db_init():
@@ -43,7 +76,7 @@ def db_init():
   cursor = conn.cursor()
 
   cursor.execute("DROP TABLE IF EXISTS my_boards")
-  cursor.execute("CREATE TABLE my_boards (name VARCHAR(255), description VARCHAR(255))")
+  cursor.execute("CREATE TABLE my_boards (name VARCHAR(255), squares VARCHAR(255) ARRAY[25])")
   cursor.close()
   conn.commit()
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Start server
+
+1. `docker-compose build --no-cache`
+1. `docker-compose up`
+1. If you make changes: `docker-compose down` `docker rm <web server and database images>`
